bc0603e4b1b0f77026fa4eaf8018f34aa6057f4d
Wilson <110548094+wilsongabriel007@users.noreply.github.com>
2024-06-06 16:49:05 -0300

    06/06

--- costs/src/App.js
+++ costs/src/App.js
@@ -1,34 +1,32 @@
-import {BrowserRouter as Router, Switch, Route,Link } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Home from './components/pages/Home';
 import Company from './components/pages/Company';
 import Contact from './components/pages/Contact';
 import NewProject from './components/pages/NewProject';
+import Container from './components/layout/container';
 
 function App() {
   return (
     <Router>
       <div>
-       <Link to="/">Home</Link>
-       <Link to="contact">Contato</Link>
-       <Link to="company">Sobre</Link>
-       <Link to="newproject">Novo Projeto</Link>
-        
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/contact">Contato</Link>
+          <Link to="/company">Sobre</Link>
+          <Link to="/newproject">Novo Projeto</Link>
+        </nav>
+
+        <Container>
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/company" element={<Company />} />
+            <Route exact path="/contact" element={<Contact />} />
+            <Route exact path="/newproject" element={<NewProject />} />
+          </Routes>
+        </Container>
+
+        <p>Footer</p>
       </div>
-      <Switch>
-        <Route exact path="/">
-            <Home />
-        </Route>
-        <Route exact path="/company">
-            <Company />
-        </Route>
-        <Route exact path="/contact">
-            <Contact />
-        </Route>
-        <Route exact path="/newproject">
-            <NewProject />
-        </Route>
-      </Switch>
-      <p>Footer</p>
     </Router>
   );
 }
---
+++ costs/src/components/layout/container.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Container.module.css';
+
+function Container(props) {
+  return (
+    <div className={styles.container}>
+      {props.children}
+    </div>
+  );
+}
+
+export default Container;
